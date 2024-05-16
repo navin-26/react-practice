6d3d7976df95216c7d87a39a6f4019c681094603
navin-26 <05navin2000@gmail.com>
2024-05-16 23:34:48 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // Switch
-import HomePage from "./pages/HomePage"; // 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage"; //
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react-practice/">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        
       </Routes>
     </BrowserRouter>
   );
